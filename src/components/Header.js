bc19bb7fc60c94a7bee32b06d90f115d4e04df36
Matt Coleman <matt.coleman@protonmail.com>
2018-08-19 16:26:43 +0100

    Formatted Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,32 +4,38 @@ import _ from 'lodash';
 import DataStore from 'flux/stores/DataStore';
 
 class Header extends React.Component {
-    render() {
-        let allPages = DataStore.getAllPages();
-        allPages = _.sortBy(allPages, [
-            function(page) {
-                return page.menu_order;
-            }
-        ]); // Sort pages by order
+  render() {
+    // Sort pages by order
+    let allPages = DataStore.getAllPages();
+    allPages = _.sortBy(allPages, [
+      function(page) {
+        return page.menu_order;
+      }
+    ]);
 
-        return (
-            <div className="header">
-                <Link to="/" style={{ marginRight: '10px' }}>
-                    Home
-                </Link>
+    return (
+      <div className="header">
+        <Link to="/" style={{ marginRight: '10px' }}>
+          Home
+        </Link>
 
-                {allPages.map(page => {
-                    if (page.slug !== 'home') {
-                        return (
-                            <Link key={page.id} to={`/${page.slug}`} style={{ marginRight: '10px' }}>
-                                {page.title.rendered}
-                            </Link>
-                        );
-                    }
-                })}
-            </div>
-        );
-    }
+        {allPages.map(page => {
+          if (page.slug !== 'home') {
+            return (
+              <Link
+                key={page.id}
+                to={`/${page.slug}`}
+                style={{ marginRight: '10px' }}
+              >
+                {page.title.rendered}
+              </Link>
+            );
+          }
+          return false;
+        })}
+      </div>
+    );
+  }
 }
 
 export default Header;
