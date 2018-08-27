80c4047b6017aab7596142cec8ef08a0addb5bd7
Matt Coleman <matt.coleman@protonmail.com>
2018-08-27 15:25:05 +0100

    Split out navigation into it's own component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,42 +1,32 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import _ from 'lodash';
-import DataStore from 'flux/stores/DataStore';
+
+import Navigation from '../Navigation';
 
 import './Header.css';
 import Image from 'images/code.jpg';
 
 class Header extends React.Component {
-  render() {
-    // Sort pages by order
-    let allPages = DataStore.getAllPages();
-    allPages = _.sortBy(allPages, [
-      function(page) {
-        return page.menu_order;
-      }
-    ]);
+  constructor() {
+    super();
+    this.state = {
+      isActive: false
+    };
+  }
 
+  openNav = () => {
+    this.setState({ isActive: !this.state.isActive });
+  };
+
+  render() {
     return (
       <div className="header">
-        <Link to="/" style={{ marginRight: '10px' }}>
-          Home
-        </Link>
-
-        {allPages.map(page => {
-          if (page.slug !== 'home') {
-            return (
-              <Link
-                key={page.id}
-                to={`/${page.slug}`}
-                style={{ marginRight: '10px' }}
-              >
-                {page.title.rendered}
-              </Link>
-            );
-          }
-          return false;
-        })}
-
+        <button
+          onClick={this.openNav}
+          style={{ position: 'relative', zIndex: 2 }}
+        >
+          Open
+        </button>
+        {this.state.isActive && <Navigation />}
         <div
           className="header-bg"
           style={{ backgroundImage: `url(${Image})` }}
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import _ from 'lodash';
+import DataStore from 'flux/stores/DataStore';
+
+import './Navigation.css';
+
+class Navigation extends React.Component {
+  render() {
+    // Sort pages by order
+    let allPages = DataStore.getAllPages();
+    allPages = _.sortBy(allPages, [
+      function(page) {
+        return page.menu_order;
+      }
+    ]);
+
+    return (
+      <nav className="navigation">
+        <Link to="/" style={{ marginRight: '10px' }}>
+          Home
+        </Link>
+
+        {allPages.map(page => {
+          if (page.slug !== 'home') {
+            return (
+              <Link
+                key={page.id}
+                to={`/${page.slug}`}
+                style={{ marginRight: '10px' }}
+              >
+                {page.title.rendered}
+              </Link>
+            );
+          }
+          return false;
+        })}
+      </nav>
+    );
+  }
+}
+
+export default Navigation;
